dad1e5698f8735921e5d34d8e4a246cae041ff61
Công Hồ <cong1772001@gmail.com>
2023-06-05 00:16:47 +0700

    commit lan 3

--- src/main/java/com/example/demoapiget/controllers/TokenController.java
+++ src/main/java/com/example/demoapiget/controllers/TokenController.java
@@ -1,30 +1,16 @@
 package com.example.demoapiget.controllers;
 
-// import javax.sql.rowset.spi.SyncResolver;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// import com.example.demoapiget.models.LoginRequest;
-import com.example.demoapiget.services.TokenService;
-
+import com.example.demoapiget.services.ContractService;
 @RestController
 public class TokenController {
     
     @Autowired
-    private TokenService tokenService;
-    @PostMapping("/token")
-    public ResponseEntity<String> getToken( ) {
-        // System.out.println(username);
-        String token = tokenService.getToken();
-        if (token != null) {
-            return ResponseEntity.ok(token);
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error retrieving token");
-        }
+    private ContractService contractService;
+    @PostMapping("/score")
+    public String getContracts( String contract,String date) {
+        return contractService.getContracts(contract, date);
     }
 }
--- src/main/java/com/example/demoapiget/services/ContractService.java
+++ src/main/java/com/example/demoapiget/services/ContractService.java
@@ -1,66 +1,78 @@
 package com.example.demoapiget.services;
 
 import java.util.Arrays;
-
-// import java.util.Arrays;
-// import java.util.List;
-
-// import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-// import org.springframework.http.HttpEntity;
-// import org.springframework.http.HttpHeaders;
-// import org.springframework.http.HttpMethod;
-// // import org.springframework.http.HttpEntity;
-// // import org.springframework.http.HttpHeaders;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.MediaType;
-// import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import com.example.demoapiget.DTO.ContractReponseModel;
 import com.example.demoapiget.DTO.LoginResponseModel;
 import com.example.demoapiget.models.ContractData;
-
-import lombok.Data;
-
+import com.example.demoapiget.models.LoginRequest;
 @Service
 public class ContractService {
         
-    @Autowired
     private RestTemplate restTemplate;
-    public String getContracts(String contract,String date) 
-    {
-        String AccessScore = null;
-        String URL = "https://cads-api.fpt.vn/smartcpe/v2/getDaiyScoreSummary";
-        try{
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            ContractData contractData = new ContractData();
-            contractData.setContract(contract);
-            contractData.setDate(date);
-            LoginResponseModel loginResponseModel = new LoginResponseModel();
-            headers.set("Authorization",loginResponseModel.getAccess_token());
-            HttpEntity<ContractData> httpEntity = new HttpEntity<ContractData>(contractData, headers);
-            ResponseEntity<ContractReponseModel> responseEntity = restTemplate.exchange(URL, HttpMethod.POST, httpEntity, ContractReponseModel.class);
-            // ContractReponseModel contractReponseModel = responseEntity.getBody();
-            AccessScore = responseEntity.getStatusCode().toString();
-            System.out.println(responseEntity.getBody().getStatus());
-            System.out.println(responseEntity.getBody().getMessages());
-            System.out.println(responseEntity.getBody().getData());
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-       
-        return AccessScore;
+
+    public ContractService() {
+        this.restTemplate = new RestTemplate();
     }
+
+        public String getContracts(String contract,String date) 
+        {
+            String accessScore = null;
+            String URL = "https://cads-api.fpt.vn/smartcpe/v2/getDaiyScoreSummary";
+            try{
+                String accessToken = getAccessToken();
+                HttpHeaders headers = new HttpHeaders();
+                headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                ContractData contractData = new ContractData();
+                contractData.setContract(contract);
+                contractData.setDate(date);
+                headers.set("Authorization",accessToken);
+                HttpEntity<ContractData> httpEntity = new HttpEntity<ContractData>(contractData, headers);
+                ResponseEntity<ContractReponseModel> responseEntity = restTemplate.exchange(URL, HttpMethod.POST, httpEntity, ContractReponseModel.class);
+                accessScore = responseEntity.getStatusCode().toString();
+                System.out.println(responseEntity.getBody().getStatus());
+                System.out.println(responseEntity.getBody().getMessages());
+                System.out.println(responseEntity.getBody().getData());
+                // System.out.println(responseEntity.getBody().());
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+           
+            return accessScore;
+        }
+        private String getAccessToken() {
+            String accessTokens = null;
+            String tokenUrl = "https://cads-api.fpt.vn/smartcpe/v2/getToken";    
+            try {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+                headers.setContentType(MediaType.APPLICATION_JSON);
+        
+                LoginRequest loginRequest = new LoginRequest();
+                loginRequest.setClientId("QC9b9h0OgstJc3eeWPWOwmpLhb6Dbux6");
+                loginRequest.setClientSecret("kM3CtxEfEj0nWrZT3DjCOd31AdAxJlcH");
+        
+                HttpEntity<LoginRequest> httpEntity = new HttpEntity<>(loginRequest, headers);
+                ResponseEntity<LoginResponseModel> responseEntity = restTemplate.exchange(tokenUrl, HttpMethod.POST, httpEntity, LoginResponseModel.class);
+                if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                    accessTokens = responseEntity.getStatusCode().toString();
+                    System.out.println(responseEntity.getBody().getAccess_token());
+                    return accessTokens = responseEntity.getBody().getAccess_token();
+                }
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            return accessTokens;
+        }
     }
 
